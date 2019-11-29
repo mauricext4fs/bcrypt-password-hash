7656f42c14181ff6e587fabcf28d76dcd1effb95
Vlad Tepes <xt4fs@iMac>
2019-11-29 23:44:51 +0100

    - Move all exit to the main processing function - Convert all dirty Println to single string - Switch the conditional statement (hahahah!!!!) - Move title inside blocks

--- src/bcrypt-password-hash/main.go
+++ src/bcrypt-password-hash/main.go
@@ -16,8 +16,8 @@ func main() {
 	// Always print the How-to
 	printHello()
 
-	// hash pass
-	if os.Args[1] == "hash" && len(os.Args) == 4 {
+	if len(os.Args) == 4 && os.Args[1] == "hash"  {
+		// hash pass
 		cost, err := strconv.Atoi(os.Args[3])
 		if err != nil {
 			log.Println(err)
@@ -26,46 +26,53 @@ func main() {
 		hash := hashAndSalt([]byte(os.Args[2]), cost)
 		fmt.Println(hash)
 		os.Exit(0)
+	} else if  len(os.Args) == 4 && os.Args[1] == "check" {
 		// check
-	} else if os.Args[1] == "check" && len(os.Args) == 4 {
 		pass := []byte(os.Args[3])
 		//fmt.Println("hash: ", string(os.Args[2]), " and pass ", string(os.Args[3]))
 		fmt.Println(comparePasswords(os.Args[2], pass))
 		os.Exit(0)
-		// Not implemented
 	} else {
-		fmt.Println("Argument invalid. Usage: ")
-		fmt.Println("./bcrypt-password-hash hash 'pass' cost")
-		fmt.Println(" - hash is the function to use")
-		fmt.Println(" - pass is the password to hash")
-		fmt.Println(" - cost (Integer) between 4 and 31 is the hardning cost to use...")
-		fmt.Println(" anything under 10 is not recommended. The higher the harder to break.")
-		fmt.Println(" ")
-		fmt.Println("./bcrypt-password-hash check 'hash' 'pass'")
-		fmt.Println(" - check is the function to use")
-		fmt.Println(" - hash is saved hash to check against")
-		fmt.Println(" - pass is the clear text password to compare with the hash")
-		fmt.Println(" ")
-		fmt.Println("In all cases an exist return value other than 0 mean that an error occurs")
-		fmt.Println(" ")
+		// No recognisable args
+		printHowTo()
 		os.Exit(0)
 	}
 }
 
 // Print How-to
 func printHello() {
-	fmt.Println(" ")
-	fmt.Println("This program use bcrypt and variable cost to generate hash for Password")
-	fmt.Println(" ")
-	fmt.Println("This implement the cutting edge and most recommend way of hash generation for password")
-	fmt.Println(" ")
-	fmt.Println("Since this program is basically compiled into bytecode it can generate ")
-	fmt.Println("very high cost hash at a very high speed")
-	fmt.Println("")
-	fmt.Println("Remember that bruteforcer password aren't usually using you cheap ")
-	fmt.Println("single thread shitty php code")
-	fmt.Println(" ")
-	fmt.Println(" ")
+	fmt.Println(`
+	This program use bcrypt and variable cost to generate hash for Password.
+
+	This implement the cutting edge and most recommend way of hash generation for password.
+
+	Since this program is basically compiled into bytecode it can generate
+	very high cost hash at a very high speed.
+
+	Remember that password bruteforcer aren't using you cheap single thread
+	php code. Therefore, you shouldn't probably use PHP for hashing.
+
+	`)
+}
+
+func printHowTo() {
+	fmt.Println(`Argument invalid.
+Usage:
+
+./bcrypt-password-hash hash 'pass' cost
+	- hash is the function to use
+	- pass is the password to hash
+	- cost (Integer) between 4 and 31 is the hardning cost to use...
+	- anything under 10 is not recommended. The higher the harder to break.
+
+./bcrypt-password-hash check 'hash' 'pass'
+	- check is the function to use
+	- hash is saved hash to check against
+	- pass is the clear text password to compare with the hash
+
+In all cases an exist return value other than 0 mean that an error occurs
+
+	`)
 }
 
 // Take Password and cost and return hash
